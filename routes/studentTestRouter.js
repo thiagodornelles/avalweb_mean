cd5b4bd114535924bbf09d68b740e50fd00e4257
thiagodornelles <thiago.azevedo87@gmail.com>
2016-10-12 16:04:24 -0300

    Teste do VS Code

--- routes/studentTestRouter.js
+++ routes/studentTestRouter.js
@@ -14,19 +14,19 @@ var StudentTestUtils = require('./studentTestUtils');
 const util = require('util');
 
 /* 
- * -----------------------------------------------------------------------------------
- * INÍCIO -- Código referente a geração de uma estrategia de aplicação de avaliação
- * -----------------------------------------------------------------------------------
- */
+* -----------------------------------------------------------------------------------
+* INÍCIO -- Código referente a geração de uma estrategia de aplicação de avaliação
+* -----------------------------------------------------------------------------------
+*/
 
 //Instanciamos o objeto que guarda a estratégia de aplicação
 var testStrat = new TestStrategy();
 
 // Implementamos uma estrategia conforme a resposta da última questão
 // e a questões do banco
-var DefaultStrategy = function(){	
-	this.nextQuestion = function(answer, questions){		
-		if(questions.length == 0){
+var DefaultStrategy = function () {
+	this.nextQuestion = function (answer, questions) {
+		if (questions.length == 0) {
 			return "end of test";
 		}
 		// console.log(questions);
@@ -41,78 +41,81 @@ var difBalance = new BalanceStrategy();
 testStrat.setStrategy(def);
 
 /*
- * -----------------------------------------------------------------------------------
- * FIM -- Código referente a geração de uma estrategia de aplicação de avaliação
- * -----------------------------------------------------------------------------------
- */
+* -----------------------------------------------------------------------------------
+* FIM -- Código referente a geração de uma estrategia de aplicação de avaliação
+* -----------------------------------------------------------------------------------
+*/
 
 
 //Funçoes de utilidade
 var stUtil = new StudentTestUtils();
 
-router.get('/', isLoggedIn, function(req, res, next){
+router.get('/', isLoggedIn, function (req, res, next) {
 	return next();
 });
 
-router.get('/id/:id', function(req, res, next){
+router.get('/id/:id', function (req, res, next) {
 	var query = testModel.findById(req.params.id);
-	query.exec(function(err, result){
+	query.exec(function (err, result) {
 		if (err)
 			res.send(err);
-		else{
+		else {
 			res.json(result);
 		}
 	});
 });
 
-router.get('/search', function(req, res, next) {		
-	var query = testModel.find({ name: new RegExp(req.query.name, 'i')}, '-questions');	
-	query.exec(function (err, result){		
-		res.json(result);		
-	});		
+router.get('/search', function (req, res, next) {
+	var query = testModel.find({ name: new RegExp(req.query.name, 'i') }, '-questions');
+	query.exec(function (err, result) {
+		res.json(result);
+	});
 });
 
 /*
- * Rota que inicia uma avaliação pelo aluno, define em sessão
- * a avaliação que está em aplicação.
- */
-router.post('/starttest', function(req, res, next) {	
+* Rota que inicia uma avaliação pelo aluno, define em sessão
+* a avaliação que está em aplicação.
+*/
+router.post('/starttest', function (req, res, next) {
 	//Validando se o teste existe	
-	var query = testModel.findById(req.body._id)	
-	.populate({ path: 'questions', select: '-answers.feedback -answers.rightAnswer' });
+	var query = testModel.findById(req.body._id)
+		.populate({ path: 'questions', select: '-answers.feedback -answers.rightAnswer' });
 
-	query.exec(function(err, result){
+	query.exec(function (err, result) {
 		if (err)
 			res.send(err);
-		else{			
+		else {
 			if (result.strategy == CONSTS.FIXED_ORDER)
 				testStrat.setStrategy(def);
 			else if (result.strategy == CONSTS.DIF_BALANCE)
 				testStrat.setStrategy(difBalance);
 
 			//Se existe, grava a informação do usuário no log da prova
-			if (result._id == req.body._id){				
+			if (result._id == req.body._id) {
 				//Solicita uma questão para começar.
-				var question;				
-				if (result.type == CONSTS.EVAL_BY_CATEGORIES){
-					if(result.categories.length > 0){						
+				var question;
+				if (result.type == CONSTS.EVAL_BY_CATEGORIES) {
+					if (result.categories.length > 0) {
 						//Popular três niveis de subCategorias e suas questões
 						var q = categoryModel.findById(result.categories[0])
-						.populate(
-							{path:'subCategories', model:'Category',
-							populate: {path:'subCategories', model:'Category'}
-						})
-						.lean();
-						q.exec(function(err, category){
+							.populate(
+							{
+								path: 'subCategories', model: 'Category',
+								populate: { path: 'subCategories', model: 'Category' }
+							})
+							.lean();
+						q.exec(function (err, category) {
 							categoryModel.populate(category,
-								{ path:'questions subCategories.questions subCategories.subCategories.questions',
-								  model:'Question', options: { lean: true }},								
-								function(err, category){									
+								{
+									path: 'questions subCategories.questions subCategories.subCategories.questions',
+									model: 'Question', options: { lean: true }
+								},
+								function (err, category) {
 									//Pega as questões na hierarquia
 									category.questions = stUtil.getQuestionsFromCategory(category);
 
-									if(category.questions.length > 0){
-										question = testStrat.nextQuestion('', category.questions);								
+									if (category.questions.length > 0) {
+										question = testStrat.nextQuestion('', category.questions);
 										var studTest = new studentTestModel();
 										studTest.user = req.session.passport.user.username;
 										studTest.test = req.body._id;
@@ -124,19 +127,19 @@ router.post('/starttest', function(req, res, next) {
 										//Envia a questão								
 										res.send(question);
 									}
-									else{
+									else {
 										res.send("end of test");
 									}
 								}
-							);							
+							);
 						});
 					}
-					else{
+					else {
 						res.send("end of test");
 					}
 				}
-				else if (result.type == CONSTS.EVAL_BY_QUESTIONS){
-					question = testStrat.nextQuestion('', result.questions);			
+				else if (result.type == CONSTS.EVAL_BY_QUESTIONS) {
+					question = testStrat.nextQuestion('', result.questions);
 					var studTest = new studentTestModel();
 					studTest.user = req.session.passport.user.username;
 					studTest.test = req.body._id;
@@ -150,175 +153,175 @@ router.post('/starttest', function(req, res, next) {
 					res.send(question);
 				}
 			}
-			else{
+			else {
 				res.send('test not started');
 			}
 		}
-	});		
+	});
 });
 
-router.post('/checkquestion', function(req, res, next){
+router.post('/checkquestion', function (req, res, next) {
 
-	if(req.body.answer_id && req.body.question_id){
+	if (req.body.answer_id && req.body.question_id) {
 		//Marcando a resposta enviada pelo usuário
 		studentTestModel.find({
 			'user': req.session.passport.user.username,
 			'test': req.body._id //ID da avaliação
 		})
-		.limit(1)
-		.sort({ 'date': -1 })
-		.exec(function(err, studTest){				
-			studTest[0].answers.push(req.body.answer_id);
-			studTest[0].save();
-		});
+			.limit(1)
+			.sort({ 'date': -1 })
+			.exec(function (err, studTest) {
+				studTest[0].answers.push(req.body.answer_id);
+				studTest[0].save();
+			});
 		//Buscando a questão na base		
 		questionModel.findById(req.body.question_id)
-		.exec(function(err, question){
-			if (question){
-				//Tranformar em um objeto normal javascript para permitir adições
-				question = question.toObject();
-				//Campo para marcar a resposta como correta se for o caso
-				question.right = false;
-				//Conferindo se a resposta está correta				
-				for (var i = 0; i < question.answers.length; i++) {					
-					if (question.answers[i]._id.toString() === req.body.answer_id &&
-						question.answers[i].rightAnswer){
-						question.right = true;						
-						break;
+			.exec(function (err, question) {
+				if (question) {
+					//Tranformar em um objeto normal javascript para permitir adições
+					question = question.toObject();
+					//Campo para marcar a resposta como correta se for o caso
+					question.right = false;
+					//Conferindo se a resposta está correta				
+					for (var i = 0; i < question.answers.length; i++) {
+						if (question.answers[i]._id.toString() === req.body.answer_id &&
+							question.answers[i].rightAnswer) {
+							question.right = true;
+							break;
+						}
 					}
-				}				
-				//Enviando a questão com as respostas e se foi acertada ou não
-				res.send(question);
-			}
-			else{
-				res.end();		
-			}		
-		});
+					//Enviando a questão com as respostas e se foi acertada ou não
+					res.send(question);
+				}
+				else {
+					res.end();
+				}
+			});
 	}
-	else{
+	else {
 		res.end();
 	}
 });
 
 /*
- * Rota que encaminha as questões
- * TODO Avaliar a modularização...
- */
+* Rota que encaminha as questões
+* TODO Avaliar a modularização...
+*/
 
-router.post('/nextquestion', function(req, res, next) {
-	if (req.body.answer_id){
+router.post('/nextquestion', function (req, res, next) {
+	if (req.body.answer_id) {
 		//Busca registro mais atual do estado de execução da avaliação
 		studentTestModel.find({
 			'user': req.session.passport.user.username,
 			'test': req.body._id //ID da avaliação
 		})
-		.limit(1).sort({ 'date': -1 })
-		.exec(function(err, studTest){
-			//Validando se encontrou registro de avaliação para este usuário
-			// console.log(studTest);
-			if (studTest.length > 0){
-				//Busca os dados das questões da avaliação
-				testModel.findById(req.body._id).populate({ path: 'questions categories' })				
-				.exec(function(err, test){
-					//console.log(test);
-					if(test){
-						//Selecionando a estratégia
-						if(test.strategy == CONSTS.FIXED_ORDER)
-							testStrat.setStrategy(def);
-						else if(test.strategy == CONSTS.DIF_BALANCE)
-							testStrat.setStrategy(difBalance);
+			.limit(1).sort({ 'date': -1 })
+			.exec(function (err, studTest) {
+				//Validando se encontrou registro de avaliação para este usuário
+				// console.log(studTest);
+				if (studTest.length > 0) {
+					//Busca os dados das questões da avaliação
+					testModel.findById(req.body._id).populate({ path: 'questions categories' })
+						.exec(function (err, test) {
+							//console.log(test);
+							if (test) {
+								//Selecionando a estratégia
+								if (test.strategy == CONSTS.FIXED_ORDER)
+									testStrat.setStrategy(def);
+								else if (test.strategy == CONSTS.DIF_BALANCE)
+									testStrat.setStrategy(difBalance);
 
-						var qtemp = test.questions;						
-						//Verificar se o aluno acertou a última questão respondida
-						answered = studTest[0].questionsAnswered;
-						var lastQuestion = answered[answered.length-1];
-						
-						//Procurando a questão respondida
-						questionModel.findById(lastQuestion)						
-						.exec(function(err, question){							
-							var rightAnswer = false;
-							var lastAnswer = studTest[0].answers[studTest[0].answers.length-1];
-							for (var i = 0; i < question.answers.length; i++) {								
-								if (question.answers[i]._id.toString() == lastAnswer.toString() &&
-									question.answers[i].rightAnswer){
-									rightAnswer = true;
-									break;
-								}
-							}
-							//----------------------
-							//  PROVA POR QUESTOES
-							//----------------------
-							if (test.type == CONSTS.EVAL_BY_QUESTIONS){
-								//Remoção das questões já respondidas
-								//As questões não respondidas serão avaliadas pelo seletor de questões
-								var questionsAns = studTest[0].questionsAnswered;
-								for (var i = qtemp.length-1 ; i > -1; i--){
-									for (var j = 0; j < questionsAns.length; j++){									
-										if (questionsAns[j].toString() == qtemp[i]._id.toString()){
-											qtemp.splice(i, 1);
-											break;		
+								var qtemp = test.questions;
+								//Verificar se o aluno acertou a última questão respondida
+								answered = studTest[0].questionsAnswered;
+								var lastQuestion = answered[answered.length - 1];
+
+								//Procurando a questão respondida
+								questionModel.findById(lastQuestion)
+									.exec(function (err, question) {
+										var rightAnswer = false;
+										var lastAnswer = studTest[0].answers[studTest[0].answers.length - 1];
+										for (var i = 0; i < question.answers.length; i++) {
+											if (question.answers[i]._id.toString() == lastAnswer.toString() &&
+												question.answers[i].rightAnswer) {
+												rightAnswer = true;
+												break;
+											}
 										}
-									}
-								}
-								//**** Buscando nova questão *****							
-								var question = testStrat.nextQuestion(rightAnswer, qtemp);							
-								
-								//String que termina o teste
-								if (question == 'end of test') {								
-									res.send(question);
-								}
-								else {
-									//Grava nova questão a responder
-									studTest[0].questionsAnswered.push(question._id);
-									studTest[0].save();
-									res.send(question);
-								}
-							}
-							//----------------------
-							// PROVA POR CATEGORIAS
-							//----------------------							
-							else if (test.type == CONSTS.EVAL_BY_CATEGORIES){
-								var cats = test.categories;
-								category.questions = stUtil.getQuestionsFromCategory(cats);
-								console.log(category.questions);
-								var question = testStrat.nextQuestion(rightAnswer, qtemp);
+										//----------------------
+										//  PROVA POR QUESTOES
+										//----------------------
+										if (test.type == CONSTS.EVAL_BY_QUESTIONS) {
+											//Remoção das questões já respondidas
+											//As questões não respondidas serão avaliadas pelo seletor de questões
+											var questionsAns = studTest[0].questionsAnswered;
+											for (var i = qtemp.length - 1; i > -1; i--) {
+												for (var j = 0; j < questionsAns.length; j++) {
+													if (questionsAns[j].toString() == qtemp[i]._id.toString()) {
+														qtemp.splice(i, 1);
+														break;
+													}
+												}
+											}
+											//**** Buscando nova questão *****							
+											var question = testStrat.nextQuestion(rightAnswer, qtemp);
 
-								res.send("end of test");							
-								
+											//String que termina o teste
+											if (question == 'end of test') {
+												res.send(question);
+											}
+											else {
+												//Grava nova questão a responder
+												studTest[0].questionsAnswered.push(question._id);
+												studTest[0].save();
+												res.send(question);
+											}
+										}
+										//----------------------
+										// PROVA POR CATEGORIAS
+										//----------------------							
+										else if (test.type == CONSTS.EVAL_BY_CATEGORIES) {
+											var cats = test.categories;
+											category.questions = stUtil.getQuestionsFromCategory(cats);
+											console.log(category.questions);
+											var question = testStrat.nextQuestion(rightAnswer, qtemp);
+
+											res.send("end of test");
+
+										}
+									});
 							}
 						});
-					}
-				});
-			}
-			else {
-				res.send(err);
-			}
-		});
+				}
+				else {
+					res.send(err);
+				}
+			});
 	}
 	else {
 		return next();
 	}
 });
 
-router.post('/', function(req, res, next) {		
-	if (req.body.name && req.body.date){
+router.post('/', function (req, res, next) {
+	if (req.body.name && req.body.date) {
 		var test = new testModel();
 		test.name = req.body.name;
 		test.date = req.body.date;
-		for(var i = 0; i < req.body.questions.length; i++){
+		for (var i = 0; i < req.body.questions.length; i++) {
 			test.questions.push(req.body.questions[i]);
-		}		
-		test.save(function(err) {
+		}
+		test.save(function (err) {
 			if (err)
 				res.send(err);
 			else
 				res.send('test saved');
-		});		
+		});
 	}
 	else {
 		res.send('test not saved');
 	}
-	
+
 });
 
 module.exports = router;
